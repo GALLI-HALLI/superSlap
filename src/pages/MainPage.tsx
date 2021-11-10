1ee25346e3fd246f9f30b0f3d50add01c492262a
김범석 <kqjatjr@gmail.com>
2021-11-11 04:00:06 +0900

    메인페이지 마크업

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,33 +1,11 @@
 import React from "react";
+import InputForm from "../components/MainPage/InputForm";
 import "../styles/MainPage.scss";
-import LOGO from "../image/LOGO.png";
 
 const MainPage = () => {
   return (
     <div className="MainPage">
-      <form>
-        <div>
-          <img className="LogoImg" alt="MainLogo" src={LOGO} />
-        </div>
-        <div className="InputContainer">
-          <p>아이디</p>
-          <input
-            className="LoginIdInputBox"
-            placeholder="아이디를 입력해 주세요"
-          />
-          <p>비밀번호</p>
-          <input
-            className="LoginPwInputBox"
-            placeholder="비밀번호를 입력해 주세요"
-          />
-        </div>
-        <div className="SubmitBtnContainer">
-          <button type="submit" className="SubmitBtn">
-            로그인
-          </button>
-          <button className="RegiBtn">회원가입</button>
-        </div>
-      </form>
+      <InputForm />
     </div>
   );
 };
