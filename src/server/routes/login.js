6cc21afe66d0bdf9c03043db769cf50b863d7d48
chae-y <58249263+chae-y@users.noreply.github.com>
2021-11-11 20:58:06 +0900

    카카오 파일 분리

--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -3,9 +3,11 @@
 //DOM-> Document Object Model
 const id = document.querySelector('#id'),
     password = document.querySelector('#password'),
-    loginBtn = document.querySelector('#login-button');
+    loginBtn = document.querySelector('#login-button'),
+    kakaoBtn = document.querySelector('#kakao-login');
 
 loginBtn.addEventListener("click", login);
+kakaoBtn.addEventListener("click", kakaoLogin);
 
 
 function login(e){
@@ -35,5 +37,43 @@ function login(e){
     .catch((err)=>{
         console.log(err);
         console.error("로그인 중 에러발생");
+    })
+}
+
+function kakaoLogin(){
+    Kakao.Auth.login({
+        
+        success: function(authObj){
+            Kakao.Auth.setAccessToken(authObj.access_token)
+            getinfo()
+            console.log(Kakao.Auth.getAccessToken())
+        },
+        fail: function(err){
+            console.log(err)
+        }
+    })
+}
+function getinfo(){
+    Kakao.API.request({
+        url: '/v2/user/me',
+        success: function(res){
+            console.log(res)
+            var nickname = res.kakao_account.profil_nickname
+            var email = res.kakao_account.email
+        },
+        fail: function(error){
+            alert('ERROR', JSON.stringify(error))
+        }
+    })
+}
+
+function kakaoLogout(){
+    if(!Kakao.Auth.getAccessToken()){
+        alert('Not logged in')
+        return
+    }
+    var token = Kakao.Auth.getAccessToken()
+    Kakao.Auth.logout(function(){
+        alert('logout: ' + token)
     })
 }
