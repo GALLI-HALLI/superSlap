ed989d4fbeb1ef63242e85fac716c7a3ded967ca
김범석 <kqjatjr@gmail.com>
2021-11-12 16:41:29 +0900

    InputForm 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import styles from "./styles/App.module.scss";
 import { Route, Switch } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import InputForm from "./components/MainPage/InputForm";
-import RegisterPage from "./components/RegisterPage/RegisterPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Route path="/">
-        <MainPage>
-          <Switch>
-            <Route path="/" exact component={InputForm} />
-            <Route path="/register" component={RegisterPage} />
-          </Switch>
-        </MainPage>
-      </Route>
+      <Switch>
+        <Route path="/" exact component={MainPage} />
+        <Route path="/register" component={RegisterPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,36 @@
 import React, { PropsWithChildren } from "react";
+import { Link } from "react-router-dom";
 import styles from "./MainPage.module.scss";
+import FormInput from "../components/MainPage/FormInput";
 import LogoImg from "../components/MainPage/LogoImg";
 
 const MainPage = ({ children }: PropsWithChildren<{}>) => {
   return (
     <div className={styles.MainPage}>
       <LogoImg />
+      <form>
+        <div className="InputContainer">
+          <FormInput
+            name="id"
+            label="아이디"
+            placeholder="아이디를 입력해 주세요"
+          />
+          <FormInput
+            name="password"
+            label="비밀번호"
+            type="password"
+            placeholder="비밀번호를 입력해 주세요"
+          />
+        </div>
+        <div className={styles.SubmitBtnContainer}>
+          <button type="submit" className={styles.SubmitBtn}>
+            로그인
+          </button>
+          <Link to="/register">
+            <button className="RegiBtn">회원가입</button>
+          </Link>
+        </div>
+      </form>
       {children}
     </div>
   );
