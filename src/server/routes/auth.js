e1d4c8eb64387731bea0fb8d16bc9e927472ab6e
username <greenrock46@gmail.com>
2021-11-13 11:07:15 +0900

    PR 피드백 반영하여 수정 - async handler, mongoose 데이터 타입 enum 처리, 필요 없는 코드 삭제

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -109,7 +109,6 @@ router.post(
       return res.json({ success: false, msg: "유효하지 않은 아이디입니다." });
     }
 
-    console.log("아이디 있음");
     let isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
