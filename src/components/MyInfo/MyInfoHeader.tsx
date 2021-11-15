06a45c83f23f75b2ad3cf0342a96780d49845c71
logan <rk172172@gmail.com>
2021-11-15 13:34:53 +0900

    css classname 변수명 카멜케이스로 변경

--- src/components/MyInfo/MyInfoHeader.tsx
+++ src/components/MyInfo/MyInfoHeader.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "../../pages/MyInfo.scss";
 import MyInfoHeaderDisplay from "./MyInfoHeaderDisplay";
-import TopStyle from "./MyInfoHeader.module.scss"
+import style from "./MyInfoHeader.module.scss"
 
 const MyInfoHeader = () => {
   return (
-    <header className="MyInfoHeader">
-      <div className={TopStyle.Top}>
-        <button className={TopStyle.Btn}>뒤로가기</button>
-        <h1 className={TopStyle.Title}> 내 정보 </h1>
-        <button className={TopStyle.Btn}>로그아웃</button>
+    <header className="myInfoHeader">
+      <div className={style.top}>
+        <button className={style.btn}>뒤로가기</button>
+        <h1 className={style.title}> 내 정보 </h1>
+        <button className={style.btn}>로그아웃</button>
       </div>
       <MyInfoHeaderDisplay />
     </header>
