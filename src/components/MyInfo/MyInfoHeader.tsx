4f450ac2334c1e1ce38d92e60a902f06fee314f7
김범석 <kqjatjr@gmail.com>
2021-11-15 16:16:44 +0900

    코드 포맷 변경

--- src/components/MyInfo/MyInfoHeader.tsx
+++ src/components/MyInfo/MyInfoHeader.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "../../pages/MyInfo.scss";
 import MyInfoHeaderDisplay from "./MyInfoHeaderDisplay";
-import style from "./MyInfoHeader.module.scss"
+import styles from "./MyInfoHeader.module.scss";
 
 const MyInfoHeader = () => {
   return (
     <header className="myInfoHeader">
-      <div className={style.top}>
-        <button className={style.btn}>뒤로가기</button>
-        <h1 className={style.title}> 내 정보 </h1>
-        <button className={style.btn}>로그아웃</button>
+      <div className={styles.top}>
+        <button className={styles.btn}>뒤로가기</button>
+        <h1 className={styles.title}> 내 정보 </h1>
+        <button className={styles.btn}>로그아웃</button>
       </div>
       <MyInfoHeaderDisplay />
     </header>
