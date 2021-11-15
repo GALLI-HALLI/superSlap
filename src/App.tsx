a88fbd57d7297219fb1a1a3ed14fcefd41c6fa73
logan <rk172172@gmail.com>
2021-11-15 13:36:36 +0900

    Route path 정리 및 내정보수정 페이지 path 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./styles/App.scss";
 import { Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import MyInfo from "./pages/MyInfo";
+import MyInfoEdit from "./components/MyInfo/MyInfoEdit";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={MainPage} />
+      <Route path="/main" component={MainPage} />
       <Route path="/my-info" component={MyInfo} />
+      <Route path="/my-info-edit" component={MyInfoEdit} />
     </div>
   );
 }
