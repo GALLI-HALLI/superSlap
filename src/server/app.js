d8323ec8206a827c1b1c624615ec4e0ac0296d0e
김범석 <kqjatjr@gmail.com>
2021-11-16 15:37:48 +0900

    로비 페이지 파일명 변경

--- src/server/app.js
+++ src/server/app.js
@@ -1,15 +1,17 @@
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
+const socket = require("socket.io");
 var http = require("http");
 
 const app = express();
 
 const server = http.createServer(app);
 
-app.use(express.static(path.join(__dirname, "../../build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", function (request, response) {
-  response.sendFile(path.join(__dirname, "../../build/index.html"));
+  response.sendFile(path.join(__dirname, "/build/index.html"));
 });
 
 server.listen(3000, function () {
