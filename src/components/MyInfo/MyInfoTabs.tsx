7ce0d35680239bee4063291065f3a73995c43360
logan <rk172172@gmail.com>
2021-11-18 00:36:44 +0900

    내 정보 페이지 css 수정

--- src/components/MyInfo/MyInfoTabs.tsx
+++ src/components/MyInfo/MyInfoTabs.tsx
@@ -3,10 +3,10 @@ import "../../pages/MyInfo.scss";
 import FirstTab from "./FirstTab";
 import SecondTab from "./SecondTab";
 import ThirdTab from "./ThirdTab";
-import cn from "classnames"
+import cn from "classnames";
 
 // 탭 이름들 가제
-const Menus = ["통계", "캐릭터 선택", "칭호"]
+const Menus = ["통계", "캐릭터", "칭호"];
 
 const MyInfoTabs = () => {
   // 어떤 탭이 처음에 보여질지.
@@ -14,34 +14,35 @@ const MyInfoTabs = () => {
 
   const WhichTabComponent = () => {
     if (activeTab === Menus[0]) {
-      return <FirstTab />
+      return <FirstTab />;
     }
     if (activeTab === Menus[1]) {
-      return <SecondTab />
+      return <SecondTab />;
     }
     if (activeTab === Menus[2]) {
-      return <ThirdTab />
+      return <ThirdTab />;
     }
-  }
+  };
 
   //  Functions to handle Tab Switching
   const handleTab = useCallback((TabName: string) => {
     setActiveTab(TabName);
   }, []);
 
-
   return (
     <div className="myInfoTabs">
       <nav className="tabNav">
-        {Menus.map(menu => <button
-          key={menu}
-          className={cn({ "active": activeTab === menu })}
-          onClick={() => handleTab(menu)}
-        >{menu}</button>)}
+        {Menus.map((menu) => (
+          <button
+            key={menu}
+            className={cn({ active: activeTab === menu })}
+            onClick={() => handleTab(menu)}
+          >
+            {menu}
+          </button>
+        ))}
       </nav>
-      <div className="TabOutlet">
-        {WhichTabComponent()}
-      </div>
+      <div className="TabOutlet">{WhichTabComponent()}</div>
     </div>
   );
 };
