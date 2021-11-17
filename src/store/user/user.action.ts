1c4a30d347a8fb58bd46c62fa26eccbb0b982556
김범석 <kqjatjr@gmail.com>
2021-11-17 23:54:29 +0900

    user 액션 설계

---
+++ src/store/user/user.action.ts
@@ -0,0 +1,33 @@
+import { createAction, Dispatch } from "@reduxjs/toolkit";
+import { getCurrentUserProfile, postCurrentUser } from "../../service/auth";
+import { TProfile, TRegister } from "../../types/api";
+
+export const registerLoading = createAction("REGISTER_LOADING");
+export const registerSuccess = createAction("REGISTER_SUCCESS");
+export const registerFailure = createAction("REGISTER_FAILURE");
+
+// 서버에서 응답하는 데이터 의미있는 값이 아닐때가 있음..
+
+export const getProfileLoading = createAction("GET_PROFILE_LOADING");
+export const getProfileSuccess = createAction<{ profile: TProfile }>(
+  "GET_PROFILE_SUCCESS",
+);
+export const getProfileFailure = createAction("GET_PROFILE_FAILURE");
+
+export const getProfile = () => async (dispatch: Dispatch) => {
+  dispatch(getProfileLoading());
+  try {
+    const profile = await getCurrentUserProfile();
+    dispatch(getProfileSuccess({ profile }));
+  } catch {
+    dispatch(getProfileFailure());
+  }
+};
+
+export const postRegister = ({user}: TRegister) => async (dispatch: Dispatch) => {
+  dispatch(registerLoading());
+  try {
+    const register = await postCurrentUser(user);
+    
+  }
+};
