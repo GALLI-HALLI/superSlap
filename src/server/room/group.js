c2f0a8c228a70f02d07a5dfe7de0962791551398
chae <stat_chae0@sookmyung.ac.kr>
2021-11-18 21:37:21 +0900

    connect 때 token으로 정보확인해서 방 들여보냄

--- src/server/room/group.js
+++ src/server/room/group.js
@@ -1,38 +1,32 @@
-const JWT = require("jsonwebtoken");
-const dotenv = require("dotenv");
-dotenv.config();
+const checkAuth = require("../middleware/checkAuth");
 
 const lobby = require("./lobby");
 const Room = require("./room");
-let roomManager = lobby.roomManager;
+const roomManager = lobby.roomManager;
 
-let TokenToInfo = async (token) => {
-  //현재 토큰에는 id만 들어있지만 곧 수정 예정
-  try {
-    let user = await JWT.verify(token, process.env.JWT_KEY);
-    return user;
-  } catch (error) {
-    return false; //false일때 어떻게 처리해야할 지 생각해봐야할듯
-  }
-};
-
-let userConnect = (socket) => {
-  let room = new Room();
+const userConnect = (socket) => {
+  let room;
   let id;
   let name;
   socket.on("enter", (code, token) => {
-    room = roomManager.rooms.get(code);
-    let user = TokenToInfo(token);
-    id = user.id;
-    name = user.name;
-    room.addPlayer(id, name);
-    socket.join(code);
+    if (!roomManager.hasRoom(code)) {
+      socket.emit("noRoom");
+    } else {
+      room = roomManager.rooms.get(code);
+      let user = checkAuth(token);
+      id = user.id;
+      name = user.name;
+      room.addPlayer(id, name);
+      socket.join(code);
+    }
   });
 
   socket.on("disconnect", () => {
-    room.removePlayer(id);
-    if (id === room.id) {
-      roomManager.destroyRoom(room.code);
+    if (room) {
+      room.removePlayer(id);
+      if (id === room.id) {
+        roomManager.destroyRoom(room.code);
+      }
     }
   });
 };
