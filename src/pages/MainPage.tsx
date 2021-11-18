e028a898b8ae6686699145b8ab748829e85bc9bf
김범석 <kqjatjr@gmail.com>
2021-11-18 21:33:42 +0900

    로그인 로직 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useState, useEffect } from "react";
 import GoogleButton from "react-google-button";
 import { Link, Redirect } from "react-router-dom";
 import styles from "./MainPage.module.scss";
@@ -7,37 +7,63 @@ import LogoImg from "../components/MainPage/LogoImg";
 import Button from "../components/common/Button";
 import useProfile from "../hooks/useProfile";
 import { AsyncActionStatus } from "../constants/redux";
+import { useDispatch } from "react-redux";
+import { userLogin } from "../store/user/user.action";
+import { useSelector } from "../hooks/typeReduxHook";
+import { useHistory } from "react-router";
 
 const googleButtonStyle = {
   width: "310px",
 };
 
 const MainPage = ({ children }: PropsWithChildren<{}>) => {
-  const { status, data } = useProfile();
+  const dispatch = useDispatch();
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
 
-  if (status === AsyncActionStatus.Success && data) {
+  const { status: userProfileStatus, data } = useProfile();
+
+  const onSubmit = (form: { id: string; password: string }) => {
+    console.log(form);
+    dispatch(userLogin(form));
+  };
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = { id, password };
+    onSubmit(data);
+  };
+
+  if (userProfileStatus === AsyncActionStatus.Success && data) {
     return <Redirect to="/lobby" push={false} />;
   }
 
   return (
     <div className={styles.mainPage}>
       <LogoImg />
-      <form>
+      <form method="POST" onSubmit={handleFormSubmit}>
         <div className={styles.inputContainer}>
           <FormInput
             name="id"
             label="아이디"
             placeholder="아이디를 입력해 주세요"
+            onChange={({ value }) => {
+              setId(value);
+            }}
           />
           <FormInput
             name="password"
             label="비밀번호"
             type="password"
             placeholder="비밀번호를 입력해 주세요"
+            onChange={({ value }) => {
+              setPassword(value);
+            }}
           />
         </div>
         <div className={styles.submitBtnContainer}>
           <Button className={styles.submitBtn}>로그인</Button>
+
           <Link to="/register">
             <Button>회원가입</Button>
           </Link>
