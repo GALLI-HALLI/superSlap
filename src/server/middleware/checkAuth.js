510e878d61dfa409d81e64f9c66cb6eb8122eb44
chae <stat_chae0@sookmyung.ac.kr>
2021-11-18 20:17:28 +0900

    방입장 추가 + make에 토큰 추가

--- src/server/middleware/checkAuth.js
+++ src/server/middleware/checkAuth.js
@@ -4,29 +4,39 @@ const JWT = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
 
-module.exports = async (req, res, next) => {
-  const token = req.header('x-auth-token');
-
-  if (!token) {
-    return res.status(403).json({
-      errors: [
-        {
-          msg: "no token found",
-        },
-      ],
-    });
-  }
+module.exports = async (token) => {
+  //현재 토큰에는 id만 들어있지만 곧 수정 예정
   try {
     let user = await JWT.verify(token, process.env.JWT_KEY);
-    req.user = user.id;
-    next();
+    return user;
   } catch (error) {
-    return res.status(400).json({
-      errors: [
-        {
-          msg: "유효하지 않은 토큰",
-        },
-      ],
-    });
+    return false; //false일때 어떻게 처리해야할 지 생각해봐야할듯
   }
 };
+
+// module.exports = async (req, res, next) => {
+//   const token = req.header('x-auth-token');
+
+//   if (!token) {
+//     return res.status(403).json({
+//       errors: [
+//         {
+//           msg: "no token found",
+//         },
+//       ],
+//     });
+//   }
+//   try {
+//     let user = await JWT.verify(token, process.env.JWT_KEY);
+//     req.user = user.id;
+//     next();
+//   } catch (error) {
+//     return res.status(400).json({
+//       errors: [
+//         {
+//           msg: "유효하지 않은 토큰",
+//         },
+//       ],
+//     });
+//   }
+// };
--- src/server/room/lobby.js
+++ src/server/room/lobby.js
@@ -2,16 +2,15 @@ const router = require("express").Router();
 const RoomManager = require("./roomManager.js");
 const { nanoid } = require("nanoid");
 
-let roomManager = new RoomManager();
+const checkAuth = require("../middleware/checkAuth");
 
-router.get("/", (req, res) => {
-  //LobbyPage render
-});
+const roomManager = new RoomManager();
 
 //방만들기 클릭하면 실행 될 것
-router.post("/", (req, res) => {
-  const id = req.body.id; //아마 토큰 받아서 토큰을 아이디로 변환해야할 것 같다-> 우선 임시로 id를 받는다
-
+router.post("/make", (req, res) => {
+  // const token = localStorage.getItem('x-auth-token');
+  const token = req.header("x-auth-token");
+  const id = checkAuth(token).id;
   let code = "";
   do {
     code = nanoid(7);
@@ -22,6 +21,20 @@ router.post("/", (req, res) => {
   res.json({
     code,
   }); //code 받아서 그 라우터로 이동(ex localhost:3000/game/code번호)
+});
+
+router.post("/enter", (req, res) => {
+  const code = req.body.code; //아마 토큰 받아서 토큰을 아이디로 변환해야할 것 같다-> 우선 임시로 id를 받는다
+
+  if (roomManager.hasRoom(code)) {
+    return res.json({
+      succuess: true,
+    });
+  } else {
+    return res.json({
+      succuess: false,
+    });
+  }
 });
 
 module.exports = {
