c5e47a560dc60ddf32da1acea4d7b2d465ccba77
김범석 <kqjatjr@gmail.com>
2021-11-18 21:31:31 +0900

    로그인 액션 구현

--- src/store/user/user.action.ts
+++ src/store/user/user.action.ts
@@ -1,10 +1,18 @@
 import { createAction, Dispatch } from "@reduxjs/toolkit";
-import { getCurrentUserProfile, signUpUser } from "../../service/auth";
-import { TProfile, TRegister } from "../../types/api";
+import {
+  getCurrentUserProfile,
+  signUpUser,
+  loginUser,
+} from "../../service/auth";
+import { TProfile, TRegister, TLogin } from "../../types/api";
 
 export const registerLoading = createAction("REGISTER_LOADING");
 export const registerSuccess = createAction("REGISTER_SUCCESS");
 export const registerFailure = createAction("REGISTER_FAILURE");
+
+export const loginLoading = createAction("LOGIN_LOADING");
+export const loginSuccess = createAction("LOGIN_SUCCESS");
+export const loginFailure = createAction("LOGIN_FAILURE");
 
 // 서버에서 응답하는 데이터 의미있는 값이 아닐때가 있음..
 
@@ -29,10 +37,28 @@ export const registerUser =
   async (dispatch: Dispatch) => {
     dispatch(registerLoading());
     try {
-      const token = await signUpUser({ id, name, password });
-      localStorage.setItem("token", token.token);
+      const { token } = await signUpUser({ id, name, password });
+      console.log(token);
+      localStorage.setItem("token", token);
       dispatch(registerSuccess());
     } catch {
       dispatch(registerFailure());
+    }
+  };
+
+export const userLogin =
+  ({ id, password }: TLogin) =>
+  async (dispatch: Dispatch) => {
+    dispatch(loginLoading());
+    try {
+      const { token } = await loginUser({ id, password });
+      if (token) {
+        localStorage.setItem("token", token);
+        dispatch(loginSuccess());
+      } else {
+        alert("아이디와 비밀번호를 다시 확인해 주세요");
+      }
+    } catch {
+      dispatch(loginFailure());
     }
   };
