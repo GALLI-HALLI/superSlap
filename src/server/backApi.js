ac6cde657baf40a8ea8cf17fa26c6cba30150e23
chae <stat_chae0@sookmyung.ac.kr>
2021-11-19 21:19:21 +0900

    닉네임수정 만드는중

--- src/server/backApi.js
+++ src/server/backApi.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const auth = require("./routes/auth");
 const lobby = require("./room/lobby");
+const userInfo = require("./routes/userInfo");
 
 router.use("/auth", auth);
 router.use("/lobby", lobby.router);
+router.use("/user-info", userInfo);
 
 module.exports = router;
--- src/server/room/lobby.js
+++ src/server/room/lobby.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const RoomManager = require("./roomManager.js");
+const asyncHandler = require("express-async-handler");
 const { nanoid } = require("nanoid");
 
 const checkAuth = require("../middleware/checkAuth");
@@ -7,6 +8,7 @@ const checkAuth = require("../middleware/checkAuth");
 const roomManager = new RoomManager();
 
 //방만들기 클릭하면 실행 될 것
+<<<<<<< HEAD
 router.post("/make", (req, res) => {
   // const token = localStorage.getItem('x-auth-token');
   const token = req.header("x-auth-token");
@@ -22,6 +24,26 @@ router.post("/make", (req, res) => {
     code,
   }); //code 받아서 그 라우터로 이동(ex localhost:3000/game/code번호)
 });
+=======
+router.post(
+  "/make",
+  asyncHandler(async (req, res) => {
+    // const token = localStorage.getItem('x-auth-token');
+    const token = req.header("x-auth-token");
+    const id = await checkAuth(token).id;
+    let code = "";
+    do {
+      code = nanoid(7);
+    } while (roomManager.hasRoom(code));
+
+    roomManager.createRoom(code, id);
+
+    res.json({
+      code,
+    }); //code 받아서 그 라우터로 이동(ex localhost:3000/game/code번호)
+  })
+);
+>>>>>>> cbef7c5 (닉네임수정 만드는중)
 
 router.post("/enter", (req, res) => {
   const code = req.body.code; //아마 토큰 받아서 토큰을 아이디로 변환해야할 것 같다-> 우선 임시로 id를 받는다
