0c87b8f3bec4550645a31e5f3e3c5a244da91355
qwbsy <qwbsy@daum.net>
2021-11-26 20:46:25 +0900

    게임종료 타임아웃 조건 설정

--- src/server/game/hunmin.js
+++ src/server/game/hunmin.js
@@ -3,32 +3,43 @@ const socketEvent = require("../constants/socketEvents");
 const request = require('request')
 
 const key = process.env.DICTIONARY_KEY
-const addr = "https://stdict.korean.go.kr/api/search.do?key=" + key + "&req_type=json&target=1&q="
+const addr = "https://stdict.korean.go.kr/api/search.do?key=" + key + "&req_type=json&q="
 
 const consonants = ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
 const wrongReason = {
     length: '글자 수를 맞춰주세요',
     already: '이미 사용한 단어입니다',
     initial: '초성이 맞지 않습니다',
-    word: '없는 단어입니다',
+    word: '표준대국어사전에 없는 단어입니다',
 }
 
 class Hunmin extends Game{
     constructor(room) {
         super(room);
         this.playerSeq = [];
+        this.len = 0;
         this.turn = 0;
         this.nowWord = ''
         this.wordList = [];
+        this.turnTimer = 0;
+        this.finish = false;
     }
     start(){
         let suggest = consonants[parseInt(Math.random()*14)] + consonants[parseInt(Math.random()*14)];
-        this.nowWord = suggest
+        this.nowWord = suggest;
+        this.len = this.playerSeq.length;
         this.playerSeq.sort((a,b)=>{return a[1]-b[1]});
+        for(let i=0; i < this.len; i++){
+            this.getRoomSocket().emit("join_user", {
+                id: this.playerSeq[i][0],
+                seq: i,
+            });
+        }
         this.getRoomSocket().emit('suggestInitial', suggest);
 
         setTimeout(()=>{
-            let loser = this.playerSeq[this.turn];
+            if(this.finish) return;
+            let loser = this.playerSeq[this.turn%this.len][0];
             this.getRoomSocket().emit(socketEvent.gameEnd, loser);
             this.comebackRoom();
         }, 2000);
@@ -41,12 +52,12 @@ class Hunmin extends Game{
         this.getRoomSocket().emit("leave_user", id);
     }
 
-    joinGame(socket, id){
-        this.playerSeq.push(id, Math.random())
+    joinGame(id){
+        this.playerSeq.push([id, Math.random()])
     }
 
     leftGame(id){
-        for(let i=0; i < this.playerSeq.length; i++){
+        for(let i=0; i < this.len; i++){
             if(this.playerSeq[i][0] === id){
                 // this.playerSeq.splice(i,1);
                 this.playerSeq[i] = false;
@@ -70,7 +81,7 @@ class Hunmin extends Game{
         return f[fn]
     }
 
-    checkWord(word){
+    async checkWord(word){
         if(word.length !== 2) return [false, wrongReason.length];
         if(this.wordList.includes(word)) return [false, wrongReason.already];
         let initial = this.getInitialConstant(word[0]) + this.getInitialConstant(word[1]) 
@@ -87,26 +98,23 @@ class Hunmin extends Game{
     initializeSocketEvents(id, socket){
         console.log(socket + ' is entered');
 
-        this.joinGame(socket, id);
+        this.joinGame(id);
 
-        socket.on('word', data=>{
-            let result = this.checkWord(data)
+        socket.on('word', async data=>{
+            let result = await this.checkWord(data)
             if(result[0]){
                 this.wordList.push(data)
-                
-                if(this.turn < this.playerSeq.length - 1){
+                do{
                     this.turn++
-                }else{
-                    this.turn = 0;
-                }
-                while(!this.playerSeq[this.turn]){
-                    if(this.turn < this.playerSeq.length - 1){
-                        this.turn++
-                    }else{
-                        this.turn = 0;
-                    }   
-                }
+                }while(!this.playerSeq[this.turn%this.len]);
                 this.getRoomSocket.emit('pass',this.turn);
+                let now = this.turn;
+                setTimeout(()=>{
+                    if(now !== this.turn) return;
+                    let loser = this.playerSeq[this.turn%this.len][0];
+                    this.getRoomSocket().emit(socketEvent.gameEnd, loser);
+                    this.comebackRoom();
+                },5000)
             }else{
                 this.getRoomSocket.emit('fail', result[1])
             }
