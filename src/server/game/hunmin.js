c32ea8dbc0926dc2ff032955acbf25af1c6b0c35
김범석 <kqjatjr@gmail.com>
2021-12-01 17:16:48 +0900

    로딩오류 버튼

--- src/server/game/hunmin.js
+++ src/server/game/hunmin.js
@@ -53,7 +53,7 @@ class Hunmin extends Game {
       let suggest =
         consonants[parseInt(Math.random() * 14)] +
         consonants[parseInt(Math.random() * 14)];
-      this.nowWord = "ㅍㄱ";
+      this.nowWord = suggest;
       this.len = this.playerSeq.length;
       this.playerSeq.sort((a, b) => {
         return a[1] - b[1];
@@ -68,7 +68,7 @@ class Hunmin extends Game {
 
       //*변경
       this.getRoomSocket().emit("setInitial", {
-        suggest: "ㅍㄱ",
+        suggest,
         gameTimeLimit: GAME_TIME_LIMIT,
         turnTimeLimit: TURN_TIME_LIMIT,
       });
